5e0bf40dc3fd5c14b575691443b78b5425c65e3d
Julian Sparber <julian@sparber.net>
2015-04-17 22:04:08 +0200

    [feat] print msg to a new line to the dom

--- main.js
+++ main.js
@@ -29,17 +29,19 @@ var input = document.getElementById('input');
 function doStuff(peer){
   peer.on('channelMessage', function (peer, channel, msg) {
     //console.log(peer, channel, msg);
-    if(msg)
-    console.log(msg.payload.data);
+    if(msg){
+      console.log(msg.payload.data);
+      document.getElementById("output").innerHTML +=  msg.payload.data + "<p>";
+    }
   });
 
   var channel = peer.getDataChannel("message", {reliable: false});
 
   channel.onopen = function(data){
-  input.addEventListener('input', function() {
+    input.addEventListener('input', function() {
       var res = peer.sendDirectly("message", "json", {data: input.value});
-    if(res)
-      console.log("Data send");
-  });
+      if(res)
+        console.log("Data send");
+    });
   };
 }
