433c65537a0725ae9a1dbc2f4bf39c447d9ee761
Julian Sparber <julian@sparber.net>
2015-04-17 19:27:36 +0200

    [save] save stuff

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
 var webrtc = new SimpleWebRTC({
-    // we don't do video
-    localVideoEl: '',
-    remoteVideosEl: '',
+  // we don't do video
+  localVideoEl: '',
+  remoteVideosEl: '',
     // dont ask for camera access
-    autoRequestMedia: false,
+  autoRequestMedia: false,
     // dont negotiate media
-    receiveMedia: {
-        mandatory: {
-            OfferToReceiveAudio: false,
-            OfferToReceiveVideo: false
-        }
+  receiveMedia: {
+    mandatory: {
+      OfferToReceiveAudio: false,
+      OfferToReceiveVideo: false
     }
+  }
 });
 
 // join without waiting for media
- webrtc.joinRoom('marco');
+webrtc.joinRoom('marco');
 
- // called when a peer is created
-  webrtc.on('createdPeer', function (peer) {
-     console.log('createdPeer', peer);
-     });
+// called when a peer is created
+webrtc.on('createdPeer', function (client) {
+  console.log('createdPeer', client);
+  document.getElementById("peers").innerHTML += client.id + "<p>";
+  doStuff(client);
+});
+
+
+  var fileinput = document.getElementById('input');
+  console.log(fileinput);
+  fileinput.type = 'file';
+  fileinput.disabled = false;
+
+ function doStuff(peer){
+  // receiving an incoming filetransfer
+  peer.on('fileTransfer', function (metadata, receiver) {
+    console.log('incoming filetransfer', metadata.name, metadata);
+    receiver.on('progress', function (bytesReceived) {
+      console.log('receive progress', bytesReceived, 'out of', metadata.size);
+    });
+    // get notified when file is done
+    receiver.on('receivedFile', function (file, metadata) {
+      console.log('received file', metadata.name, metadata.size);
+
+      // close the channel
+      receiver.channel.close();
+    });
+    filelist.appendChild(item);
+  });
+
+
+
+  fileinput.addEventListener('change', function() {
+    fileinput.disabled = true;
+
+    var file = fileinput.files[0];
+    var sender = peer.sendFile(file);
+  });
+  }
