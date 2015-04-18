418aae29d6fac79c01c1d21bef74defe074f67c9
Julian Sparber <julian@sparber.net>
2015-04-18 14:41:40 +0200

    [feat] remote controle for the canvas dot

--- main.js
+++ main.js
@@ -17,34 +17,52 @@ var webrtc = new SimpleWebRTC({
 webrtc.joinRoom('marco');
 
 // called when a peer is created
-webrtc.on('createdPeer', function (client) {
-	console.log('createdPeer', client);
-	document.getElementById("peers").innerHTML += client.id + "<p>";
-	doStuff(client);
-});
-
-
-var input = document.getElementById('input');
-
-function doStuff(peer){
+webrtc.on('createdPeer', function (peer) {
+	console.log('createdPeer', peer);
+	var div = document.getElementById("peers");
+	peer.getDataChannel("message");
+	var allPeers = webrtc.webrtc.peers;
+	//handle incoming data
 	peer.on('channelMessage', function (peer, channel, msg) {
-		//console.log(peer, channel, msg);
-		if(msg){
-			console.log(msg.payload.data);
-			document.getElementById("output").innerHTML +=  msg.payload.data + "<p>";
+		// check type, has to be json
+		if(msg.type === "json"){
+			console.log(msg.payload);
+			doStuff(msg.payload);
 		}
 	});
 
-	var channel = peer.getDataChannel("message", {reliable: false});
+	for (var i = 0; i < allPeers.length; i++)
+		div.innerHTML = allPeers[i].id + "</p>";
+});
 
-	channel.onopen = function(data){
-		input.addEventListener('input', function() {
-			var res = peer.sendDirectly("message", "json", {data: input.value});
-			if(res)
-				console.log("Data send");
-		});
-	};
+
+//send a json to a peer
+function sendPeer(peer, data){
+	//create data channel
+	var channel = peer.getDataChannel("message");
+
+	var isOpen = peer.sendDirectly("message", "json", data);
+	if(isOpen)
+		console.log("Data send");
+	else
+		console.log("Problem sending data");
 }
+
+//send a json to all peers
+function sendAll(data){
+	var peers = webrtc.webrtc.peers;
+	for (var i = 0; i < peers.length; i++) {
+		sendPeer(peers[i], data);
+	}
+}
+
+function doStuff(data){
+	x = data.x;
+	y = data.y;
+	ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+	ctx.fillRect(data.x % canvas.width, data.y % canvas.height, 10, 10);
+}
+
 
 //canvas
 
@@ -59,7 +77,7 @@ ctx.fillRect(0, 0, 10, 10);
 
 //add listener
 document.onkeydown = function(e){
-	var step = 5;
+	var step = 1;
 	switch (e.keyCode){
 		// left arrow
 		case 37: 
@@ -80,6 +98,7 @@ document.onkeydown = function(e){
 	}
 	if (x < 0) x = canvas.width;
 	if (y < 0) y = canvas.height;
+	sendAll({x: x, y: y});
 	ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
 	ctx.fillRect(x % canvas.width, y % canvas.height, 10, 10);
 }
